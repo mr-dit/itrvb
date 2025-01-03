ae60c2534ec1227c6f548c4b34e79d1011227bec
d.mezentsev <d.mezentsev@agropromcifra.ru>
2025-01-03 16:36:14 +0300

    fix: испр namespace

--- src/interfaces/CommentsRepositoryInterface.php
+++ src/interfaces/CommentsRepositoryInterface.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace App;
+namespace App\Interfaces;
+
+use App\Comment;
+use App\UUID;
 
 interface CommentsRepositoryInterface
 {
--- src/interfaces/PostsRepositoryInterface.php
+++ src/interfaces/PostsRepositoryInterface.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace App;
+namespace App\Interfaces;
+
+use App\Post;
+use App\UUID;
 
 interface PostsRepositoryInterface
 {
