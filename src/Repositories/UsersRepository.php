7b6827f8cdf0b75a492810d692bb05db8607cca7
d.mezentsev <d.mezentsev@agropromcifra.ru>
2025-01-04 16:30:51 +0300

    feat: доб userRepository

---
+++ src/Repositories/UsersRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+use App\User;
+use App\UUID;
+use App\Exceptions\UserNotFoundException;
+use Error;
+use PDO;
+use PDOException;
+
+class UsersRepository
+{
+  public function __construct(
+    private PDO $pdo
+  ) {}
+  public function get(UUID $uuid): User
+  {
+
+    $stmt = $this->pdo->prepare("SELECT * FROM users WHERE uuid = :uuid");
+
+    try {
+      $stmt->execute([
+        ":uuid" => $uuid
+      ]);
+
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+      if (!$result) {
+        throw new UserNotFoundException();
+      }
+    } catch (PDOException $e) {
+      throw new Error("Ошибка при получении пользователя: " . $e->getMessage());
+    }
+
+    return new User(
+      $result['uuid'],
+      $result['username'],
+      $result['nickname'],
+      $result['first_name'],
+      $result['last_name']
+    );
+  }
+}
